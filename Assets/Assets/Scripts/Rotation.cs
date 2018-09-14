ceb94fdde403bdaf8f3d3acf73e8b56d31de9a8c
Ben Hurst <hurstbenj@ecs.vuw.ac.nz>
2018-09-15 01:19:53 +1200

    Updated angle detection
    Player angle marker now follows mouse accurately.

--- Assets/Assets/Scripts/Rotation.cs
+++ Assets/Assets/Scripts/Rotation.cs
@@ -6,8 +6,10 @@ public class Rotation : MonoBehaviour {
     
     public Vector3 dir;
     public float angle;
-
-    float jumpForce;
+    float jumpForce = 5;
+    
+    Vector3 playerPosition, mousePosition, p, v;
+    Ray r;
 
     bool angleRestriction;
 
@@ -19,43 +21,40 @@ public class Rotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
-        jumpForce = Random.Range(0, 10);
+        playerPosition = GameObject.FindWithTag("p1").transform.position;
+        mousePosition = Input.mousePosition;
+        p = Camera.main.WorldToScreenPoint(playerPosition);
+        angle = getAngle(mousePosition,p);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().AddForce(Vector2.up * 3, ForceMode.Impulse);
+            GetComponent<Rigidbody>().AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("PlayerPosition:" + p + ", mousePosition:" + mousePosition);
+            Debug.Log("Angle:" + getAngle(mousePosition,p));
+
         }
 
-
-        if (angle <= 180 && angle >= 0)
+        if (angle < 180 && angle > 0)
         {
-            angleRestriction = false;
-        }
-        else
-        {
-            angleRestriction = true;
-        }
-
-        if (angleRestriction == false)
-        {
-            dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            print("test");
+        }
+        else if (angle >= 180 || angle < -90)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180);
         }
         else
         {
-            dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            if (transform.rotation.z < 0 && transform.rotation.z > -90)
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-            }
-            else if (angle < - 90 || transform.rotation.z > 180)
-            {
-                transform.eulerAngles = new Vector3(0,0, 180);
-            }
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
+    }
+
+    //Returns the angle between the mouse and the centre of 
+    private float getAngle(Vector3 point1, Vector3 point2)
+    {
+        dir = point1 - point2;
+        return Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
     }
 }
