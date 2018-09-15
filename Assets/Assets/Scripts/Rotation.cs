f71859e975e4fd02078ee25cdff8cc5140aa7f19
Ben Hurst <hurstbenj@ecs.vuw.ac.nz>
2018-09-15 20:57:42 +1200

    Implemented better force line and jump physics
    have created a new object to deal with showing the jump direction and have implemented directional force jumping. Has yet to be cleaned up.

--- Assets/Assets/Scripts/Rotation.cs
+++ Assets/Assets/Scripts/Rotation.cs
@@ -8,34 +8,28 @@ public class Rotation : MonoBehaviour {
     public float angle;
     float jumpForce = 5;
     
-    Vector3 playerPosition, mousePosition, p, v;
+    Vector3 playerPosition, mousePosition, p, force;
     Ray r;
+    
 
     bool angleRestriction;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start ()
+    {
 
-        playerPosition = GameObject.FindWithTag("p1").transform.position;
-        mousePosition = Input.mousePosition;
+    }
+
+    private Vector3 ip, cp;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        playerPosition = transform.position;
+        mousePosition = (Vector3) GetCurrentMousePosition();
         p = Camera.main.WorldToScreenPoint(playerPosition);
         angle = getAngle(mousePosition,p);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GetComponent<Rigidbody>().AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
-        }
-        else if (Input.GetKeyDown(KeyCode.P))
-        {
-            Debug.Log("PlayerPosition:" + p + ", mousePosition:" + mousePosition);
-            Debug.Log("Angle:" + getAngle(mousePosition,p));
-
-        }
+        force = mousePosition - playerPosition;
 
         if (angle < 180 && angle > 0)
         {
@@ -49,6 +43,24 @@ public class Rotation : MonoBehaviour {
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
+
+        //Debugging code
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Applying " + force + " force.");
+            GetComponent<Rigidbody>().AddForce(force * jumpForce, ForceMode.Impulse);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("PlayerPosition:" + playerPosition + ", mousePosition:" + mousePosition);
+            Debug.Log("Angle:" + getAngle(mousePosition,p));
+
+            float i = Mathf.Abs(mousePosition.x - p.x);
+
+            Debug.Log("Difference in X : " + i);
+
+        }
+
     }
 
     //Returns the angle between the mouse and the centre of 
@@ -56,5 +68,18 @@ public class Rotation : MonoBehaviour {
     {
         dir = point1 - point2;
         return Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+    }
+
+    private Vector3? GetCurrentMousePosition()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var plane = new Plane(Vector3.forward, Vector3.zero);
+
+        float rayDistance;
+        if (plane.Raycast(ray, out rayDistance))
+        {
+            return ray.GetPoint(rayDistance);
+        }
+        return null;
     }
 }
