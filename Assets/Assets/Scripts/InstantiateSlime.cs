a859542fdaee65ada9293e24661fdb5932f9f1c3
Unknown <robert.a.nickel@gmail.com>
2018-09-16 16:50:14 +1200

    FinalOutput

---
+++ Assets/Assets/Scripts/InstantiateSlime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateSlime : MonoBehaviour {
+    public GameObject smallSlime;
+    GameObject slimeClone1, slimeClone2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "point")
+        {
+            Destroy(gameObject);
+            slimeClone1 = Instantiate(smallSlime, new Vector3(-19.43f, 1.9f, 0), Quaternion.identity);
+            slimeClone2 = Instantiate(smallSlime, new Vector3(-17.95f, 1.9f, 0), Quaternion.identity);
+        }
+    }
+}
